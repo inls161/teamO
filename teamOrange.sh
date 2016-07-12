ce41b94845afdda9325ff105c918b9a80cba8958
lumiwille <lumiwille@gmail.com>
2016-07-12 12:35:46 -0400

    our bash file with questions

---
+++ teamOrange.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "How old are you?"
+read age
+echo "How many pets do you have?"
+read pets
+echo "How many siblings do you have?"
+read siblings
+echo "How many planets are in the solar system?"
+read planets
+echo "Do you like ice cream? (yes/no)"
+read icecream
+
+echo $age > TeamOrangeAnswer.list 
+echo $pets >> TeamOrangeAnswer.list
+echo $siblings >> TeamOrangeAnswer.list
+echo $planets >> TeamOrangeAnswer.list
+echo $icecream >> TeamOrangeAnswer.list
+paste -d, -s TeamOrangeAnswer.list > TeamOrangeAnswer.csv
+rm TeamOrangeAnswer.list
